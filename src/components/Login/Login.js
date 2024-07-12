ad899ce1300931c1a4bc625dc894655b948839bb
Your Name <you@example.com>
2024-07-12 16:54:58 +0700

    form login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import './Login.scss'
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            <div className="container">
+                <div className="row px-3 px-sm-3">
+                    <div className="content-left d-none col-sm-7 d-sm-block">
+                        <div className='brand'>
+                            Facebook
+                        </div>
+                        <div className='detail'>
+                            Facebook helps you connect and share with the people in your life.
+                        </div>
+                    </div>
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+                        <div className='brand d-sm-none'>
+                            Facebook
+                        </div>
+                        <input type='text' className='form-control'placeholder='Email address or phone number'/>
+                        <input type='password' className='form-control'placeholder='Password'/>
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>
+                            <a className='forgot-password' href='/#'>
+                                Forgotten password?
+                            </a>
+                        </span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
