d052bc7ec72bcedd1a619f954efdcf6d58dee1d7
huynh <huynhnguyen120202@gmail.com>
2024-08-07 15:19:38 +0700

    10.6.1

--- src/services/userService.js
+++ src/services/userService.js
@@ -12,6 +12,10 @@ const loginUser = (valueLogin, password) => {
     })
 }
 
+const fetchAllUser = () => {
+    return axios.get('http://localhost:8080/api/v1/user/read')
+}
+
 export {
-    registerNewUser, loginUser
+    registerNewUser, loginUser, fetchAllUser
 }
