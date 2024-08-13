5bb1505d882e8ff8a2acf7f365cbe954406c841c
huynh <huynhnguyen120202@gmail.com>
2024-08-13 23:15:40 +0700

    10.7 Customize Axios

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,7 +36,7 @@ const Login = (props) => {
 
         let response = await loginUser(valueLogin, password);
 
-        if (response && response.data && +response.data.EC === 0) {
+        if (response && +response.EC === 0) {
             // success
             let data = {
                 isAuthenticated: true,
@@ -48,9 +48,9 @@ const Login = (props) => {
             //redux
         }
 
-        if (response && response.data && +response.data.EC !== 0) {
+        if (response &&  +response.EC !== 0) {
             // error
-            toast.error(response.data.EM)
+            toast.error(response.EM)
         }
     }
 
