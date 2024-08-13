1efc00d175a3dd70ff233b968477181cb69a9bd1
huynh <huynhnguyen120202@gmail.com>
2024-08-13 22:26:03 +0700

    10.6.7 CRUD complete

--- src/components/ManageUsers/ModalUser.js
+++ src/components/ManageUsers/ModalUser.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { fetchGroup, createNewUser } from '../../services/userService';
+import { fetchGroup, createNewUser, updateCurrentUser } from '../../services/userService';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import _ from 'lodash';
@@ -33,7 +33,7 @@ const ModalUser = (props) => {
     const [userGroups, setUserGroups] = useState([]);
 
     useEffect(() => {
-        getGroups();
+        // getGroups();
     }, [])
 
     useEffect(() => {
@@ -70,6 +70,7 @@ const ModalUser = (props) => {
     }
 
     const checkValidateInputs = () => {
+        if (action === 'UPDATE') return true;
         setValidInputs(validInputsDefault);
         let arr = ['email', 'phone', 'password', 'group'];
         let check = true;
@@ -89,10 +90,15 @@ const ModalUser = (props) => {
     const handleConfirmUser = async () => {
         let check = checkValidateInputs();
         if (check === true) {
-            let res = await createNewUser({...userData, groupId: userData['group']});
+            let res = action === 'CREATE' ?
+            await createNewUser({...userData, groupId: userData['group']})
+            : await updateCurrentUser({...userData, groupId: userData['group']});
             if (res.data && res.data.EC === 0) {
                 props.onHide();
-                setUserData({...defaultUserData, group: userGroups[0].id});
+                setUserData({
+                    ...defaultUserData,
+                    group: userGroups && userGroups.length > 0 ? userGroups[0].id : ''
+                });
             } 
             if (res.data && res.data.EC !== 0) {
                 toast.error(res.data.EM);
