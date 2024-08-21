847b4ce118266ae540ae79c9d47af8f8139ba011
huynh <huynhnguyen120202@gmail.com>
2024-08-21 21:29:21 +0700

    11.10 Loading Page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import Nav from './components/Navigation/Nav';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { Rings } from "react-loader-spinner";
+import { useContext } from "react";
+import { UserContext } from "./contextt/UserContext";
 
 function App() {
+  const { user } = useContext(UserContext)
   return (
     <>
       <Router>
-        <div className="app-header">
-          <Nav />
-        </div>
-        <div className="app-container">
-          <AppRoutes />
-        </div>
+        {user && user.isLoading ?
+          <div className="loading-container">
+            <Rings
+              visible={true}
+              height="80"
+              width="80"
+              color="#1877f2"
+              ariaLabel="rings-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+            <div>Loading data...</div>
+          </div>
+          :
+          <>
+            <div className="app-header">
+              <Nav />
+            </div>
+            <div className="app-container">
+              <AppRoutes />
+            </div>
+          </>
+        }
 
         <ToastContainer
         position="bottom-center"
